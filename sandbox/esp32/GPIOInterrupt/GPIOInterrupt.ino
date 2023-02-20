a0bd43c0e2f25a0f638743f6990520a75c6029b3
Wes Cravens <wcravens@cortex-it.com>
2023-02-20 13:40:05 -0600

    Merge changes for other computer.

--- sandbox/esp32/GPIOInterrupt/GPIOInterrupt.ino
+++ sandbox/esp32/GPIOInterrupt/GPIOInterrupt.ino
@@ -1,49 +1,73 @@
 #include <chrono>
 #include <Arduino.h>
 
+#define DEBOUNCE_MILLISECONDS 100
+
+unsigned long debounce_counter = 0;
 
 struct Button {
     const uint8_t PIN;
     uint32_t numberKeyPresses;
     bool pressed;
+    unsigned long millis_since_last_interrupt;
 };
 
-Button button1 = {5, 0, false};
-Button button2 = {11, 0, false};
+Button button1 = {11, 0, false};
+Button button2 = {5, 0, false};
 
+/*
 void ARDUINO_ISR_ATTR isr(void* arg) {
     Button* s = static_cast<Button*>(arg);
+    if( millis() - s->millis_since_last_interrrupt < DEBOUNCE_MILLISECONDS ) return; 
     s->numberKeyPresses += 1;
+    s->millis_since_last_interrrupt = millis();
     s->pressed = true;
 }
+*/
 
-void ARDUINO_ISR_ATTR isr() {
+void ARDUINO_ISR_ATTR button_1_isr() {
+    if( millis() - debounce_counter < DEBOUNCE_MILLISECONDS ) return; 
+    button1.numberKeyPresses += 1;
+    debounce_counter = millis();
+    button1.pressed = true;
+}
+void ARDUINO_ISR_ATTR button_2_isr() {
+    if( millis() - debounce_counter < DEBOUNCE_MILLISECONDS ) return; 
     button2.numberKeyPresses += 1;
+    debounce_counter = millis();
     button2.pressed = true;
 }
+
 
 void setup() {
     auto start = std::chrono::steady_clock::now();
     Serial.begin(115200);
+
     pinMode(button1.PIN, INPUT_PULLUP);
-    attachInterruptArg(button1.PIN, isr, &button1, FALLING);
+    attachInterrupt( digitalPinToInterrupt( button1.PIN ), button_1_isr, FALLING );
+
     pinMode(button2.PIN, INPUT_PULLUP);
-    attachInterrupt(button2.PIN, isr, FALLING);
+    attachInterrupt( digitalPinToInterrupt( button2.PIN ), button_2_isr, FALLING );
 }
 
 void loop() {
     if (button1.pressed) {
         Serial.printf("Button 1 has been pressed %u times\n", button1.numberKeyPresses);
+        Serial.println( button1.millis_since_last_interrupt );
         button1.pressed = false;
     }
     if (button2.pressed) {
         Serial.printf("Button 2 has been pressed %u times\n", button2.numberKeyPresses);
+        Serial.println( button2.millis_since_last_interrupt );
         button2.pressed = false;
     }
+    /*
     static uint32_t lastMillis = 0;
     if (millis() - lastMillis > 10000) {
       lastMillis = millis();
-      detachInterrupt(button1.PIN);
+      detachInterrupt(digitalPinToInterrupt( button1.PIN ) );
+      Serial.println( "Detached button1.PIN\n");
     }
+    */
 }
 
