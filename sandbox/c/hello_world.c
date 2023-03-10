328bf4cefb2bf7edc2894bf00089749f444a9a2d
Wes Cravens <wcravens@cortex-it.com>
2023-03-10 02:35:04 -0600

    Add come void pointer usage to hello_world

--- sandbox/c/hello_world.c
+++ sandbox/c/hello_world.c
@@ -1,8 +1,20 @@
 // hello.c
 #include <stdio.h>
 
+struct Foo {
+  int num;
+  char bar;
+} foo;
+
+
 int main() {
+  foo.num = 23;
+  foo.bar = 'a';
+  void * ptr = &foo;
+
+  struct Foo baz = *(struct Foo*) ptr;
   printf( "Hello, world!\n" );
+  printf( "%c\n", baz.bar );
   return 0;
 }
 
