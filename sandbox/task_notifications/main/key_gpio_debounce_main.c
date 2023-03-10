60d9904c957c38108e4caa6e3ddcd8e00cd9335f
Wes Cravens <wcravens@cortex-it.com>
2023-03-10 06:26:54 -0600

    Task notifications POC is working.

--- sandbox/task_notifications/main/key_gpio_debounce_main.c
+++ sandbox/task_notifications/main/key_gpio_debounce_main.c
@@ -13,18 +13,28 @@
 #define GPIO_INPUT_PIN_SEL ( ( 1ULL<<GPIO_LEFT_PADDLE ) | ( 1ULL<<GPIO_RIGHT_PADDLE ) );
 
 static QueueHandle_t gpio_evt_queue = NULL;
+static TaskHandle_t  gpio_task_handle; 
 
 static void gpio_isr_handler( void* arg ) {
-    uint32_t gpio_num = (uint32_t) arg;
-    xQueueSendFromISR(gpio_evt_queue, &gpio_num, NULL);
+    BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+    uint32_t gpioPin = (uint32_t) arg;
+    xTaskNotifyFromISR( gpio_task_handle, gpioPin, eSetValueWithOverwrite, &xHigherPriorityTaskWoken );
+    portYIELD_FROM_ISR( xHigherPriorityTaskWoken );
 }
 
-static void gpio_task_example(void* arg) {
-    uint32_t io_num;
+static void gpio_task_handler(void* pvParameters) {
+    BaseType_t xEvent;
+    uint32_t ulNotifiedValue;
+    uint32_t lastLeftTick;
+    uint32_t lastRightTick;
     for(;;) {
-        if( xQueueReceive( gpio_evt_queue, &io_num, portMAX_DELAY ) ) {
-            printf( "GPIO[%"PRIu32"] intr, val: %d\n", io_num, gpio_get_level( io_num ) );
-        }
+      xEvent = xTaskNotifyWait( 0x00, ULONG_MAX, &ulNotifiedValue, portMAX_DELAY );
+      if( xEvent == pdPASS ) {
+        TickType_t now = xTaskGetTickCount();
+        uint32_t pinNum = ulNotifiedValue;
+        uint32_t pinState = gpio_get_level( pinNum );
+        printf( "Tick: %ld, GPIO Pin: %ld, State: %ld. \n", now, pinNum, pinState );
+      }
     }
 }
 
@@ -40,13 +50,11 @@ void configure_input_gpio() {
 
 void configure_event_management() {
   gpio_evt_queue = xQueueCreate(10, sizeof(uint32_t));
-  xTaskCreate( gpio_task_example, "gpio_task_example", 2048, NULL, 10, NULL );
+  xTaskCreate( gpio_task_handler, "gpio_task_example", 2048, NULL, 10, &gpio_task_handle );
   gpio_install_isr_service( ESP_INTR_FLAG_EDGE );
   gpio_isr_handler_add( GPIO_RIGHT_PADDLE,  gpio_isr_handler, (void*) GPIO_RIGHT_PADDLE );
   gpio_isr_handler_add( GPIO_LEFT_PADDLE,   gpio_isr_handler, (void*) GPIO_LEFT_PADDLE  );
 }
-
-
 
 void app_main( void ) {
   configure_input_gpio();
