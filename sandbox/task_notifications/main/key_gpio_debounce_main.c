82f911d15b3a27b952b95825154f35989d8341d3
Wes Cravens <wcravens@cortex-it.com>
2023-03-10 03:44:46 -0600

    Add Task Notification example project.

--- sandbox/task_notifications/main/key_gpio_debounce_main.c
+++ sandbox/task_notifications/main/key_gpio_debounce_main.c
@@ -12,40 +12,20 @@
 #define GPIO_RIGHT_PADDLE 38
 #define GPIO_INPUT_PIN_SEL ( ( 1ULL<<GPIO_LEFT_PADDLE ) | ( 1ULL<<GPIO_RIGHT_PADDLE ) );
 
-struct gpioInteruptMessage {
-  gpio_num_t gpio_num;
-  uint32_t level;
-  TickType_t lastChangedOnTick;
-};
+static QueueHandle_t gpio_evt_queue = NULL;
 
-int64_t now() {
-    struct timeval tv_now;
-    gettimeofday(&tv_now, NULL);
-    return (int64_t)tv_now.tv_sec * 1000000L + (int64_t)tv_now.tv_usec;
+static void gpio_isr_handler( void* arg ) {
+    uint32_t gpio_num = (uint32_t) arg;
+    xQueueSendFromISR(gpio_evt_queue, &gpio_num, NULL);
 }
 
-volatile TickType_t lastISR = 0;
-
-static void IRAM_ATTR gpio_isr_handler( void* arg ) {
-  gpio_num_t pinNum = *(gpio_num_t *) arg;
-  struct gpioInteruptMessage gpioState;
-  gpioState.gpio_num = pinNum;
-  gpioState.level = gpio_get_level( pinNum );
-  gpioState.lastChangedOnTick = xTaskGetTickCount();
-  xQueueSendFromISR( gpio_evt_queue, &gpioState, NULL );
-}
-
-static void gpio_task_example( void* arg ) {
-  struct gpioInteruptMessage state;
-  for(;;) {
-    if ( xQueueReceive( gpio_evt_queue, &state, portMAX_DELAY ) ) {
-      TickType_t nowTick = xTaskGetTickCount();
-      printf( "GPIO[%d] intr, level: %ld, lastChangedOnTick: %lu, currentTick: %lu\n", state.gpio_num, state.level, state.lastChangedOnTick, nowTick );
-      if ( nowTick - lastISR > 10 ) {
-        lastISR = nowTick;
-      }
+static void gpio_task_example(void* arg) {
+    uint32_t io_num;
+    for(;;) {
+        if( xQueueReceive( gpio_evt_queue, &io_num, portMAX_DELAY ) ) {
+            printf( "GPIO[%"PRIu32"] intr, val: %d\n", io_num, gpio_get_level( io_num ) );
+        }
     }
-  }
 }
 
 void configure_input_gpio() {
@@ -58,21 +38,17 @@ void configure_input_gpio() {
   gpio_config( &io_conf );
 }
 
-struct gpioInteruptMessage left_paddle;
-struct gpioInteruptMessage right_paddle;
-static QueueHandle_t gpio_evt_queue = NULL;
-
-void setup_isr_queue(){
-  gpio_evt_queue = xQueueCreate( 1, sizeof( struct gpioInteruptMessage ) );
+void configure_event_management() {
+  gpio_evt_queue = xQueueCreate(10, sizeof(uint32_t));
   xTaskCreate( gpio_task_example, "gpio_task_example", 2048, NULL, 10, NULL );
   gpio_install_isr_service( ESP_INTR_FLAG_EDGE );
-  gpio_isr_handler_add( right_paddle.gpio_num,  gpio_isr_handler, (void*) &right_paddle.gpio_num );
-  gpio_isr_handler_add( left_paddle.gpio_num,   gpio_isr_handler, (void*) &left_paddle.gpio_num );
+  gpio_isr_handler_add( GPIO_RIGHT_PADDLE,  gpio_isr_handler, (void*) GPIO_RIGHT_PADDLE );
+  gpio_isr_handler_add( GPIO_LEFT_PADDLE,   gpio_isr_handler, (void*) GPIO_LEFT_PADDLE  );
 }
 
+
+
 void app_main( void ) {
-  right_paddle.gpio_num = GPIO_RIGHT_PADDLE;
-  left_paddle.gpio_num = GPIO_LEFT_PADDLE;
   configure_input_gpio();
-  setup_isr_queue();
+  configure_event_management();
 }
